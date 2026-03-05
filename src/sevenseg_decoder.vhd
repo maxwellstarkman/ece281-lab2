4753be1fc64536c23bd3fe202ef69d8d3d1cb709
maxwellstarkman <c28maxwell.starkman@afacademy.af.edu>
2026-03-04 21:08:36 -0700

    Fix?

--- src/sevenseg_decoder.vhd
+++ src/sevenseg_decoder.vhd
@@ -1,22 +1,5 @@
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
-entity sevenseg_decoder is
-    Port ( i_Hex : in STD_LOGIC_VECTOR (3 downto 0);
-           o_seg_n : out STD_LOGIC_VECTOR (6 downto 0));
-end sevenseg_decoder;
-
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+library ieee;
+use ieee.std_logic_1164.all;
 
 entity sevenseg_decoder is
     Port ( i_Hex : in STD_LOGIC_VECTOR (3 downto 0);
@@ -26,21 +9,21 @@ end sevenseg_decoder;
 architecture Behavioral of sevenseg_decoder is
 begin
     with i_Hex select
-        o_seg_n <= "1000000" when "0000",
-                   "1111001" when "0001",
-                   "0100100" when "0010",
-                   "0110000" when "0011",
-                   "0011001" when "0100",
-                   "0010010" when "0101",
-                   "0000010" when "0110",
-                   "1111000" when "0111",
-                   "0000000" when "1000",
-                   "0011000" when "1001",
-                   "0001000" when "1010",
-                   "0000011" when "1011",
-                   "1000110" when "1100",
-                   "0100001" when "1101",
-                   "0000110" when "1110",
-                   "0001110" when "1111",
+        o_seg_n <= "1000000" when x"0",
+                   "1111001" when x"1",
+                   "0100100" when x"2",
+                   "0110000" when x"3",
+                   "0011001" when x"4",
+                   "0010010" when x"5",
+                   "0000010" when x"6",
+                   "1111000" when x"7",
+                   "0000000" when x"8",
+                   "0011000" when x"9",
+                   "0001000" when x"A",
+                   "0000011" when x"B",
+                   "0100111" when x"C",
+                   "0100001" when x"D",
+                   "0000110" when x"E",
+                   "0001110" when x"F",
                    "1111111" when others;
 end Behavioral;
--- src/sevenseg_decoder_tb.vhd
+++ src/sevenseg_decoder_tb.vhd
@@ -1,51 +1,58 @@
 library ieee;
-  use ieee.std_logic_1164.all;
-  use ieee.numeric_std.all;
+use ieee.std_logic_1164.all;
 
 entity sevenseg_decoder_tb is
 end sevenseg_decoder_tb;
 
-architecture test_bench of sevenseg_decoder_tb is 
-    
+architecture Behavioral of sevenseg_decoder_tb is
     component sevenseg_decoder is
-        port ( 
-            i_Hex   : in  std_logic_vector (3 downto 0);
-            o_seg_n : out std_logic_vector (6 downto 0)
-        );
+    Port ( i_Hex : in STD_LOGIC_VECTOR (3 downto 0);
+           o_seg_n : out STD_LOGIC_VECTOR (6 downto 0));
     end component;
 
-    signal w_hex : std_logic_vector(3 downto 0) := x"0";
-    signal w_seg : std_logic_vector(6 downto 0);
+    signal w_hex : std_logic_vector(3 downto 0) := (others => '0');
+    signal w_seg_n : std_logic_vector(6 downto 0);
 
 begin
-    sevenseg_decoder_uut : sevenseg_decoder 
-    port map (
-        i_Hex   => w_hex,
-        o_seg_n => w_seg
+    sevenseg_decoder_inst : sevenseg_decoder port map (
+        i_Hex => w_hex,
+        o_seg_n => w_seg_n
     );
-    
-    test_process : process 
+
+    test_process : process
     begin
         w_hex <= x"0"; wait for 10 ns;
-        assert (w_seg = "1000000") report "Fail: 0" severity failure;
-        
+        assert w_seg_n = "1000000" report "Error on 0" severity error;
         w_hex <= x"1"; wait for 10 ns;
-        assert (w_seg = "1111001") report "Fail: 1" severity failure;
-        
+        assert w_seg_n = "1111001" report "Error on 1" severity error;
+        w_hex <= x"2"; wait for 10 ns;
+        assert w_seg_n = "0100100" report "Error on 2" severity error;
+        w_hex <= x"3"; wait for 10 ns;
+        assert w_seg_n = "0110000" report "Error on 3" severity error;
+        w_hex <= x"4"; wait for 10 ns;
+        assert w_seg_n = "0011001" report "Error on 4" severity error;
+        w_hex <= x"5"; wait for 10 ns;
+        assert w_seg_n = "0010010" report "Error on 5" severity error;
+        w_hex <= x"6"; wait for 10 ns;
+        assert w_seg_n = "0000010" report "Error on 6" severity error;
+        w_hex <= x"7"; wait for 10 ns;
+        assert w_seg_n = "1111000" report "Error on 7" severity error;
         w_hex <= x"8"; wait for 10 ns;
-        assert (w_seg = "0000000") report "Fail: 8" severity failure;
-
+        assert w_seg_n = "0000000" report "Error on 8" severity error;
         w_hex <= x"9"; wait for 10 ns;
-        assert (w_seg = "0011000") report "Fail: 9" severity failure;
-        
+        assert w_seg_n = "0011000" report "Error on 9" severity error;
         w_hex <= x"A"; wait for 10 ns;
-        assert (w_seg = "0001000") report "Fail: A" severity failure;
-        
+        assert w_seg_n = "0001000" report "Error on A" severity error;
+        w_hex <= x"B"; wait for 10 ns;
+        assert w_seg_n = "0000011" report "Error on B" severity error;
+        w_hex <= x"C"; wait for 10 ns;
+        assert w_seg_n = "0100111" report "Error on C" severity error;
+        w_hex <= x"D"; wait for 10 ns;
+        assert w_seg_n = "0100001" report "Error on D" severity error;
+        w_hex <= x"E"; wait for 10 ns;
+        assert w_seg_n = "0000110" report "Error on E" severity error;
         w_hex <= x"F"; wait for 10 ns;
-        assert (w_seg = "0001110") report "Fail: F" severity failure;
-
-        report "Seven Segment Decoder Testbench Complete";
+        assert w_seg_n = "0001110" report "Error on F" severity error;
         wait;
-    end process;    
-    
-end test_bench;
+    end process;
+end Behavioral;
